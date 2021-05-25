88c157e0be07ef6000977e8a655df6dadfc3e169
Dorongov Alexandr <ADorongov@beeline.kz>
2021-05-25 15:00:23 +0600

    module4 solution adding

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,15 +1,15 @@
 (function () {
 
-  var namesList = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
   
-  for (var i = 0; i < namesList.length; i++) {
-    var firstLetter = namesList[i].charAt(0).toLowerCase();
+  for (var i = 0; i < names.length; i++) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
   
     if (firstLetter === 'j') {
-      byeSpeaker(namesList[i]);
+      byeSpeaker(names[i]);
     }
     else {
-      helloSpeaker(namesList[i]);
+      helloSpeaker(names[i]);
     }
   }
   
